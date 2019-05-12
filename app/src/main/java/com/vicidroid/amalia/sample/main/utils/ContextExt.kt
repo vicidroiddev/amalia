42afd23f185ae9ff89465d22d907ca1a76caf33f
Raphael <raphael@carezone.com>
2019-05-12 13:20:15 -0700

    Add toast context utils

--- app/src/main/java/com/vicidroid/amalia/sample/main/utils/ContextExt.kt
+++ app/src/main/java/com/vicidroid/amalia/sample/main/utils/ContextExt.kt
@@ -4,6 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 fun Context.inflate(layout: Int, root: View? = null, attach: Boolean = false) =
-        LayoutInflater.from(this).inflate(layout, root as ViewGroup?, attach)
+    LayoutInflater.from(this).inflate(layout, root as ViewGroup?, attach)
+
+fun Context.toastShort(msg: String) =
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).also { it.show() }
+
+fun Context.toastLong(msg: String) =
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).also { it.show() }
