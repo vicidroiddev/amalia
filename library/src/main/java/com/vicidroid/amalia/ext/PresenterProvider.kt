7f3282cd46fc913acf060a39c772a031635c2378
Raphael <raphael@carezone.com>
2019-05-12 20:38:15 -0700

    Formatting

--- library/src/main/java/com/vicidroid/amalia/ext/PresenterProvider.kt
+++ library/src/main/java/com/vicidroid/amalia/ext/PresenterProvider.kt
@@ -20,22 +20,26 @@ import com.vicidroid.amalia.core.BasePresenter
  */
 inline fun <reified P : BasePresenter<*, *>> Fragment.presenterProvider(
     noinline hooks: ((P) -> Unit)? = null,
-    crossinline presenterCreator: () -> P) =
-    presenterProvider(this, presenterCreator,  hooks)
+    crossinline presenterCreator: () -> P
+) =
+    presenterProvider(this, presenterCreator, hooks)
 
 inline fun <reified P : BasePresenter<*, *>> FragmentActivity.presenterProvider(
     noinline hooks: ((P) -> Unit)? = null,
-    crossinline presenterCreator: () -> P) =
+    crossinline presenterCreator: () -> P
+) =
     presenterProvider(this, presenterCreator, hooks)
 
 inline fun <reified P : BasePresenter<*, *>> AppCompatActivity.presenterProvider(
     noinline hooks: ((P) -> Unit)? = null,
-    crossinline presenterCreator: () -> P) =
+    crossinline presenterCreator: () -> P
+) =
     presenterProvider(this, presenterCreator, hooks)
 
 inline fun <reified P : BasePresenter<*, *>> LifecycleOwner.presenterProvider(
     noinline hooks: ((P) -> Unit)? = null,
-    crossinline presenterCreator: () -> P) =
+    crossinline presenterCreator: () -> P
+) =
     presenterProvider(this, presenterCreator, hooks)
 
 /**
@@ -44,14 +48,15 @@ inline fun <reified P : BasePresenter<*, *>> LifecycleOwner.presenterProvider(
  * It would be ideal to leverage this in [#onBindViewDelegate(...)]
  */
 inline fun <reified P : BasePresenter<*, *>> BasePresenter<*, *>.childPresenterProvider(
-    crossinline presenterCreator: () -> P) = lazy {
-      viewLifecycleOwner ?: error("The parent presenter must be bound to a view delegate.")
-      presenterLifecycleOwner ?: error("The parent presenter must have been initialized.")
-      presenterCreator().also { childPresenter ->
-          childPresenter.applicationContext = applicationContext
-          childPresenter.presenterLifecycleOwner = presenterLifecycleOwner
-      }
-  }
+    crossinline presenterCreator: () -> P
+) = lazy {
+    viewLifecycleOwner ?: error("The parent presenter must be bound to a view delegate.")
+    presenterLifecycleOwner ?: error("The parent presenter must have been initialized.")
+    presenterCreator().also { childPresenter ->
+        childPresenter.applicationContext = applicationContext
+        childPresenter.presenterLifecycleOwner = presenterLifecycleOwner
+    }
+}
 
 /**
  * The persistence of this presenter will depend on the lifecycle owner.
@@ -65,7 +70,8 @@ inline fun <reified P : BasePresenter<*, *>> BasePresenter<*, *>.childPresenterP
 inline fun <reified P : BasePresenter<*, *>> presenterProvider(
     lifecycleOwner: LifecycleOwner,
     crossinline presenterCreator: () -> P,
-    noinline externalHooks: ((P) -> Unit)? = null) = lazy(LazyThreadSafetyMode.NONE) {
+    noinline externalHooks: ((P) -> Unit)? = null
+) = lazy(LazyThreadSafetyMode.NONE) {
 
   @Suppress("UNCHECKED_CAST")
   val factory = object : ViewModelProvider.Factory {
@@ -75,25 +81,23 @@ inline fun <reified P : BasePresenter<*, *>> presenterProvider(
           presenter.applicationContext = lifecycleOwner.applicationContext
           externalHooks?.invoke(it)
         }
-      }
     }
-  }
 
-  when (lifecycleOwner) {
-    is FragmentActivity -> ViewModelProviders.of(lifecycleOwner, factory)[P::class.java]
-    is Fragment -> ViewModelProviders.of(lifecycleOwner, factory)[P::class.java]
-    else -> error("Unsupported lifecycle owner detected.")
-  }.also { p ->
-      p.presenterLifecycleOwner = lifecycleOwner
-  }
+    when (lifecycleOwner) {
+        is FragmentActivity -> ViewModelProviders.of(lifecycleOwner, savedStateFactory)[P::class.java]
+        is Fragment -> ViewModelProviders.of(lifecycleOwner, savedStateFactory)[P::class.java]
+        else -> error("Unsupported lifecycle owner detected.")
+    }.also { p ->
+        p.presenterLifecycleOwner = lifecycleOwner
+    }
 }
 
 val LifecycleOwner.applicationContext: Context
-  get() = when (this) {
-    is FragmentActivity -> this.application
-    is Fragment -> this.activity!!.application
-    else -> error("Unable to obtain context due to unsupported lifecycle owner.")
-  }
+    get() = when (this) {
+        is FragmentActivity -> this.application
+        is Fragment -> this.activity!!.application
+        else -> error("Unable to obtain context due to unsupported lifecycle owner.")
+    }
 
 /**
  * Workaround for reified types which allows the use of [presenterProvider] in Java.
@@ -101,9 +105,9 @@ val LifecycleOwner.applicationContext: Context
  */
 
 object PresenterProvider {
-  @Suppress("UNCHECKED_CAST")
-  @JvmStatic
-  fun <P : BasePresenter<*,*>> provide(fragment: Fragment, presenterCreator: () -> BasePresenter<*, *>): P {
-    return fragment.presenterProvider { presenterCreator() }.value as P
-  }
+    @Suppress("UNCHECKED_CAST")
+    @JvmStatic
+    fun <P : BasePresenter<*, *>> provide(fragment: Fragment, presenterCreator: () -> BasePresenter<*, *>): P {
+        return fragment.presenterProvider { presenterCreator() }.value as P
+    }
 }
