763b0f573e33967d813ab18d8340fcb8e8d2e42d
Raphael <raphael@carezone.com>
2019-06-09 09:55:16 -0400

    Format BaseViewDelegate

--- library/src/main/java/com/vicidroid/amalia/ui/BaseViewDelegate.kt
+++ library/src/main/java/com/vicidroid/amalia/ui/BaseViewDelegate.kt
@@ -19,93 +19,93 @@ abstract class BaseViewDelegate<S : ViewState, E : ViewEvent>(
     val lifecycleOwner: LifecycleOwner,
     val rootView: View,
     injectLayoutId: Int? = null,
-    rootViewAnchorId: Int = R.id.amalia_stub)
-  : LifecycleOwner {
+    rootViewAnchorId: Int = R.id.amalia_stub
+) : LifecycleOwner {
 
-  private var hostActivity: AppCompatActivity? = null
+    private var hostActivity: AppCompatActivity? = null
 
-  val context: Context = rootView.context
+    val context: Context = rootView.context
 
-  /**
-   * The parent view delegate is accessible from a child when using [com.vicidroid.amalia.ext.viewDelegateProvider]
-   */
-  var parent: BaseViewDelegate<*, *>? = null
+    /**
+     * The parent view delegate is accessible from a child when using [com.vicidroid.amalia.ext.viewDelegateProvider]
+     */
+    var parent: BaseViewDelegate<*, *>? = null
 
-  init {
-    injectLayoutId?.let { layoutId ->
-      findViewById<ViewStub>(rootViewAnchorId).apply {
-        layoutResource = layoutId
-        inflate()
-      }
+    init {
+        injectLayoutId?.let { layoutId ->
+            findViewById<ViewStub>(rootViewAnchorId).apply {
+                layoutResource = layoutId
+                inflate()
+            }
+        }
     }
-  }
 
-  /**
-   * Emits view events that should be processed by #onEvent from a presenter.
-   */
-  private val eventLiveData = MutableLiveData<E>()
+    /**
+     * Emits view events that should be processed by #onEvent from a presenter.
+     */
+    private val eventLiveData = MutableLiveData<E>()
 
-  /**
-   * Convenience method which uses [rootView] as the parent for view traversal
-   */
-  fun <T : View> findViewById(@IdRes id: Int): T = rootView.findViewById(id)
+    /**
+     * Convenience method which uses [rootView] as the parent for view traversal
+     */
+    fun <T : View> findViewById(@IdRes id: Int): T = rootView.findViewById(id)
 
-  /**
-   * Lazily attempts to find view using [rootView] as the parent for view traversal.
-   * This can be used to avoid setting views immediately upon initialization of the view delegate.
-   */
-  fun <T : View> findViewByIdLazy(@IdRes id: Int) = lazy<T> { rootView.findViewById(id) }
+    /**
+     * Lazily attempts to find view using [rootView] as the parent for view traversal.
+     * This can be used to avoid setting views immediately upon initialization of the view delegate.
+     */
+    fun <T : View> findViewByIdLazy(@IdRes id: Int) = lazy<T> { rootView.findViewById(id) }
 
-  /**
-   * Convenience method which uses the [context] obtained from the [rootView]
-   */
-  fun getString(@StringRes id: Int) = context.getString(id)
+    /**
+     * Convenience method which uses the [context] obtained from the [rootView]
+     */
+    fun getString(@StringRes id: Int) = context.getString(id)
 
-  /**
-   * Exposes view event live data that the view delegate can leverage to reflect UI changes.
-   */
-  fun eventLiveData(): LiveData<E> = eventLiveData
+    /**
+     * Exposes view event live data that the view delegate can leverage to reflect UI changes.
+     */
+    fun eventLiveData(): LiveData<E> = eventLiveData
 
-  /**
-   * Allows view delegates to know about events that are sent.
-   * Parent view delegates may use this to intercept events sent from child delegates.
-   */
-  fun propagateEventsTo(observer: (E) -> Unit) =
-      eventLiveData().observe(lifecycleOwner, Observer { observer(it) })
+    /**
+     * Allows view delegates to know about events that are sent.
+     * Parent view delegates may use this to intercept events sent from child delegates.
+     */
+    fun propagateEventsTo(observer: (E) -> Unit) =
+        eventLiveData().observe(lifecycleOwner, Observer { observer(it) })
 
-  /**
-   * Exposes the lifecycle from the [lifecycleOwner]
-   */
-  override fun getLifecycle() = lifecycleOwner.lifecycle
+    /**
+     * Exposes the lifecycle from the [lifecycleOwner]
+     */
+    override fun getLifecycle() = lifecycleOwner.lifecycle
 
-  /**
-   * Sends event from some interaction or UI change to an active subscriber (Presenter)
-   */
-  fun pushEvent(event: E) {
-    eventLiveData.value = event
-  }
+    /**
+     * Sends event from some interaction or UI change to an active subscriber (Presenter)
+     */
+    fun pushEvent(event: E) {
+        eventLiveData.value = event
+    }
 
-  /**
-   * Render a view state that is provided by the Presenter.
-   * The view delegate updates the UI accordingly.
-   */
-  abstract fun renderViewState(state: S)
+    /**
+     * Render a view state that is provided by the Presenter.
+     * The view delegate updates the UI accordingly.
+     */
+    abstract fun renderViewState(state: S)
 
-  fun toast(id: Int) {
-    Toast.makeText(context, id, Toast.LENGTH_LONG).show()
-  }
+    fun toast(id: Int) {
+        Toast.makeText(context, id, Toast.LENGTH_LONG).show()
+    }
 
-  fun setHostActivity(activity: AppCompatActivity) {
-    hostActivity?.let { "Duplicate setting of host activity is suspicious." }
-    hostActivity = activity
-  }
+    fun setHostActivity(activity: AppCompatActivity) {
+        hostActivity?.let { "Duplicate setting of host activity is suspicious." }
+        hostActivity = activity
+    }
 
-  /**
-   * Recursively go up to the parent view delegate to determine the host activity
-   * which is injected using [com.vicidroid.amalia.ext.viewDelegateProvider]]
-   */
-  fun hostActivity() : AppCompatActivity = when (parent) {
-    null -> hostActivity!!
-    else -> parent!!.hostActivity()
-  }
+    /**
+     * Recursively go up to the parent view delegate to determine the host activity
+     * which is injected using [com.vicidroid.amalia.ext.viewDelegateProvider]]
+     */
+    fun hostActivity(): AppCompatActivity = when (parent) {
+        null -> hostActivity!!
+        else -> parent!!.hostActivity()
+    }
 }
