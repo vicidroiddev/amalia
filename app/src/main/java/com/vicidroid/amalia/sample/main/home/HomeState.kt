c9383ce3982ca7177d7181b06fff311979ff5145
Raphael <raphael@carezone.com>
2019-06-15 09:11:00 -0700

    Make Home loaded state parceable
    This will be useful to automatically restore view states

--- app/src/main/java/com/vicidroid/amalia/sample/main/home/HomeState.kt
+++ app/src/main/java/com/vicidroid/amalia/sample/main/home/HomeState.kt
@@ -1,7 +1,10 @@
 package com.vicidroid.amalia.sample.main.home
 
+import android.os.Parcelable
 import com.vicidroid.amalia.core.ViewState
+import kotlinx.android.parcel.Parcelize
 
-sealed class HomeState : ViewState {
-    class Loaded(val data: String, val imageUrl: String) : HomeState()
+sealed class HomeState : ViewState, Parcelable {
+    @Parcelize
+    data class Loaded(val data: String, val imageUrl: String) : HomeState()
 }
