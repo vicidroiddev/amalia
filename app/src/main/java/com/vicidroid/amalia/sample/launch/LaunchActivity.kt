fbe9d96fd1aab958afff47d60ec392fdaecda7a1
Raphael <raphael@carezone.com>
2019-07-06 18:30:55 -0700

    Ensure app theme is applied to MainActivity

--- app/src/main/java/com/vicidroid/amalia/sample/launch/LaunchActivity.kt
+++ app/src/main/java/com/vicidroid/amalia/sample/launch/LaunchActivity.kt
@@ -6,7 +6,6 @@ import com.vicidroid.amalia.sample.BaseActivity
 import com.vicidroid.amalia.sample.R
 import com.vicidroid.amalia.sample.main.MainActivity
 import com.vicidroid.amalia.sample.utils.startActivityClazz
-import com.vicidroid.amalia.sample.utils.toastLong
 import kotlinx.android.synthetic.main.activity_launch.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -16,24 +15,22 @@ class LaunchActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
 
-        fullscreenContent.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//        fullscreenContent.systemUiVisibility =
+//            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+//                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+//                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+//                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+//                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
 
         launch {
-            // This doesn't work well for Samsung
-//            for (count in 1..10) {
-//                toastLong(count.toString())
-//                delay(200)
-//            }
+            delay(1000)
 
             startActivityClazz(MainActivity::class.java)
             finishAfterTransition()
