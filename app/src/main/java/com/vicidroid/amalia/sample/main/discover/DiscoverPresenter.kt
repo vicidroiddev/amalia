182d3a0caa499007d565c43e58f19e718438310d
Raphael <raphael@carezone.com>
2019-07-08 10:25:55 -0700

    Define coroutine scopes for discover presenter

--- app/src/main/java/com/vicidroid/amalia/sample/main/discover/DiscoverPresenter.kt
+++ app/src/main/java/com/vicidroid/amalia/sample/main/discover/DiscoverPresenter.kt
@@ -4,16 +4,18 @@ import com.vicidroid.amalia.core.BasePresenter
 import com.vicidroid.amalia.ext.debugLog
 import com.vicidroid.amalia.sample.api.themoviedb.discover.DiscoverRepository
 import com.vicidroid.amalia.sample.main.dashboard.Refreshable
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 
-class DiscoverPresenter(private val repository: DiscoverRepository) : BasePresenter<DiscoverState, DiscoverEvent>(),
+class DiscoverPresenter(private val repository: DiscoverRepository) :
+    BasePresenter<DiscoverState, DiscoverEvent>(),
     Refreshable {
 
     override fun loadInitialState() {
         debugLog(TAG_INSTANCE, "loadInitialState()")
-        GlobalScope.launch(Dispatchers.Main) {
+        mainScope.launch(Dispatchers.Main) {
             val results = repository.discoverFromApi()
             results.forEach { debugLog(TAG_INSTANCE, it.toString()) }
             pushState(DiscoverState.Loaded(results))
@@ -21,5 +23,16 @@ class DiscoverPresenter(private val repository: DiscoverRepository) : BasePresen
     }
 
     override fun onRefreshRequest() {
+    }
+
+    //TODO, generalize this, could perhaps use the logic of viewModelScope.
+    //https://github.com/vicidroiddev/amalia/issues/12
+    private val supervisorJob = SupervisorJob()
+
+    private val mainScope = CoroutineScope(Dispatchers.Main + supervisorJob)
+
+    override fun onCleared() {
+        supervisorJob.cancel()
+        super.onCleared()
     }
 }
