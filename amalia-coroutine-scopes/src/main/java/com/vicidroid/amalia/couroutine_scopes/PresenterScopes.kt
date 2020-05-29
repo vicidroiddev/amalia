348828ef16a8df6de6a6002a4aa5462bda028cbc
ViciDroid <vicidroiddev@gmail.com>
2020-05-29 15:40:09 -0700

    Update api/implementation specifications in gradle files

--- amalia-coroutine-scopes/src/main/java/com/vicidroid/amalia/couroutine_scopes/PresenterScopes.kt
+++ amalia-coroutine-scopes/src/main/java/com/vicidroid/amalia/couroutine_scopes/PresenterScopes.kt
@@ -1,4 +1,4 @@
-package androidx.lifecycle //we need to be in this package to access getTag in ViewModel
+package com.vicidroid.amalia.couroutine_scopes
 
 import com.vicidroid.amalia.core.BasePresenter
 import kotlinx.coroutines.*
@@ -8,6 +8,7 @@ import kotlin.coroutines.CoroutineContext
 private const val MAIN_SCOPE_KEY = "com.vicidroid.amalia.core.PresenterScopes:MainScope.JOB_KEY"
 private const val IO_SCOPE_KEY = "com.vicidroid.amalia.core.PresenterScopes:IOScope.JOB_KEY"
 private const val DEFAULT_SCOPE_KEY = "com.vicidroid.amalia.core.PresenterScopes:DefaultScope.JOB_KEY"
+private const val IMMEDIATE_SCOPE_KEY = "com.vicidroid.amalia.core.PresenterScopes:ImmediateScope.JOB_KEY"
 
 /**
  * A coroutine scope which uses [Dispatchers.Main]
@@ -16,6 +17,11 @@ private const val DEFAULT_SCOPE_KEY = "com.vicidroid.amalia.core.PresenterScopes
 val BasePresenter.mainScope: CoroutineScope
     get() {
         return scopeFor(Dispatchers.Main)
+    }
+
+val BasePresenter.immediate: CoroutineScope
+    get() {
+        return scopeFor(Dispatchers.Main.immediate)
     }
 
 val BasePresenter.ioScope: CoroutineScope
@@ -33,11 +39,12 @@ private fun BasePresenter.scopeFor(dispatcher: CoroutineDispatcher): CoroutineSc
         Dispatchers.Main -> MAIN_SCOPE_KEY
         Dispatchers.IO -> IO_SCOPE_KEY
         Dispatchers.Default -> DEFAULT_SCOPE_KEY
+        Dispatchers.Main.immediate -> IMMEDIATE_SCOPE_KEY
         else -> error("Unknown dispatcher provided")
     }
 
     return this.closeableObjects.getOrPut(key) {
-        CloseableCoroutineScope(SupervisorJob() + dispatcher)
+        AmaliaCloseableCoroutineScope(SupervisorJob() + dispatcher)
     } as CoroutineScope
 }
 
@@ -45,7 +52,7 @@ private fun BasePresenter.scopeFor(dispatcher: CoroutineDispatcher): CoroutineSc
  * A CoroutineScope that implements [java.io.Closable]
  * Viewmodels will go through the `mBagOfTags` and call close as long as the object implements [Closable]
  */
-private class CloseableCoroutineScope(
+private class AmaliaCloseableCoroutineScope(
     override val coroutineContext: CoroutineContext
 ) : Closeable,
     CoroutineScope {
