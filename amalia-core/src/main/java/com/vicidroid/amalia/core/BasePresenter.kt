3e97dbfaf86f173dc0c6d217ef00c9d0fb63439e
ViciDroid <vicidroiddev@gmail.com>
2022-12-15 13:20:44 -0500

    Fix spelling

--- amalia-core/src/main/java/com/vicidroid/amalia/core/BasePresenter.kt
+++ amalia-core/src/main/java/com/vicidroid/amalia/core/BasePresenter.kt
@@ -370,7 +370,7 @@ abstract class BasePresenter : ViewModel(),
     /**
      * Ensures collectors no longer receive new ephemeral states.
      * Resets the ephemeral state back to null.
-     * Null values are not emmited by default.
+     * Null values are not emitted by default.
      */
     private fun resetEphemeralState() {
         // Prevent leaks, view should no longer collect from this state flow on the same scope
